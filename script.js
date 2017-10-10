cc85cf70659e5678ef380c9b67587f78b918bdd2
BI4HUU <BI4HUU@yandex.ru>
2017-10-10 15:11:43 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,31 @@
-setInterval(() => {
-	document.getElementById('wrapperSpanSlide').style.height = 	`${document.getElementById('wrapperSpanSlide').offsetWidth/1.8}px`
-}, 100);
-var numberNextSlide = 1;
-var numberBeakSlide = 4;
-var numberBackSlide;
-var zI = 1;
-var slide1 = document.getElementById('slide1');
-var slide2 = document.getElementById('slide2');
-var slide3 = document.getElementById('slide3');
-var slide4 = document.getElementById('slide4');
-var s1 = document.getElementById(`slide${numberNextSlide}`);
-var s2 = document.getElementById(`slide${numberBeakSlide}`); 
+let time = 4000;
+let speed = 2.8;
+var numberNextSlide = 1;  // Номер наступного слайда
+var numberBeakSlide = 4; // Номер основного слайда
+var numberBackSlide;     // Номер попереднього слайда
+var zI = 1;                 // z-index
+function Id(e) {return document.getElementById(e)};
+var s1 = Id(`slide${numberNextSlide}`);
+var s2 = Id(`slide${numberBeakSlide}`); 
 var setI = setInterval(() => {
 	setTimeout(() => nextAuto(), 8);
 	setTimeout(() => f3(), 88);
-}, 7500);
-setI;
+}, time);setI;
 function nextAuto() {
 	numberNextSlide++;
 	numberBeakSlide = numberNextSlide-1;
 	if (numberBeakSlide == 0) {numberBeakSlide = 4};
 	if(numberNextSlide == 5){numberNextSlide = 1};
-	s1 = document.getElementById(`slide${numberNextSlide}`);
-	s2 = document.getElementById(`slide${numberBeakSlide}`); 
+	s1 = Id(`slide${numberNextSlide}`);
+	s2 = Id(`slide${numberBeakSlide}`); 
 	s1.style.cssText="transition: 'left 0s';left: 100%;";s1.style.zIndex = `${zI}`;
 	s2.style.cssText="transition: 'left 0s';left: 0%;";s2.style.zIndex = `${zI}`;
 	zI++;
 }; 
 function f3() {
-	s1.style.transition = "left .8s cubic-bezier(.35,.47,.93,.92)";
+	s1.style.transition = `left ${speed}s cubic-bezier(.35,.47,.93,.92)`;
 	s1.style.left = "0%";
-	s2.style.transition = "left .8s cubic-bezier(.35,.47,.93,.92)";
+	s2.style.transition = `left ${speed}s cubic-bezier(.35,.47,.93,.92)`;
 	s2.style.left = "-100%";
 };
 function clearIntervalMini() {
@@ -38,7 +33,7 @@ function clearIntervalMini() {
 	setI = setInterval(() => {
 		setTimeout(() => nextAuto(), 8);
 		setTimeout(() => f3(), 88);
-	}, 7500);
+	}, time);
 	setI;
 };
 function next() {
@@ -48,7 +43,7 @@ function next() {
 function nextAuto1(nNSU) {
 	numberNextSlide=nNSU;
 	s2 = s1;
-	s1 = document.getElementById(`slide${numberNextSlide}`);
+	s1 = Id(`slide${numberNextSlide}`);
 	s1.style.cssText="transition: 'left 0s';left: 100%;";s1.style.zIndex = `${zI}`;
 	s2.style.cssText="transition: 'left 0s';left: 0%;";s2.style.zIndex = `${zI}`;
 	zI++;
@@ -72,7 +67,7 @@ function nextBack(nNSU) {
 		numberNextSlide--;
 		if (numberNextSlide==0) {numberNextSlide=4};
 		s2 = s1;
-		s1 = document.getElementById(`slide${numberNextSlide}`);
+		s1 = Id(`slide${numberNextSlide}`);
 		s1.style.cssText="transition: 'left 0s';left: -100%;";s1.style.zIndex = `${zI}`;
 		s2.style.cssText="transition: 'left 0s';left: 0%;";s2.style.zIndex = `${zI}`;
 		zI++;
@@ -80,16 +75,16 @@ function nextBack(nNSU) {
 		numberNextSlide = nNSU;
 		if (numberNextSlide==0) {numberNextSlide=4};
 		s2 = s1;
-		s1 = document.getElementById(`slide${numberNextSlide}`);
+		s1 = Id(`slide${numberNextSlide}`);
 		s1.style.cssText="transition: 'left 0s';left: -100%;";s1.style.zIndex = `${zI}`;
 		s2.style.cssText="transition: 'left 0s';left: 0%;";s2.style.zIndex = `${zI}`;
 		zI++;
 	};
 };
 function f3B() {
-	s1.style.transition = "left .8s cubic-bezier(.35,.47,.93,.92)";
+	s1.style.transition = `left ${speed}s cubic-bezier(.35,.47,.93,.92)`;
 	s1.style.left = "0%";
-	s2.style.transition = "left .8s cubic-bezier(.35,.47,.93,.92)";
+	s2.style.transition = `left ${speed}s cubic-bezier(.35,.47,.93,.92)`;
 	s2.style.left = " 100%";
 };
 function lS() {
@@ -112,3 +107,9 @@ function lS() {
 // }; 
       
 //---------------
+// let Id = ['slide1', 'slide2', 'slide3', 'slide4'];
+// for (let i = 0; i < Id.length; i++) {eval(`var ${Id[i]} = document.getElementById(${Id[i]})`)};
+// var slide1 = document.getElementById('slide1');
+// var slide2 = document.getElementById('slide2');
+// var slide3 = document.getElementById('slide3');
+// var slide4 = document.getElementById('slide4');
