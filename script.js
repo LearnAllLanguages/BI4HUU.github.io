f841bf3de2b915a2205c98d8f11989aff145bc64
BI4HUU <BI4HUU@yandex.ru>
2017-10-16 15:25:11 +0400

    Add files via upload
    FINAL1

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
-// добавить автоховер на кнопки
-
 let time = 8000; // Пауза між кадрами (мілісек)
 let speed = 1;  // скорость листання (сек)
 function Id(e) {return document.getElementById(e)};
 function CE(e) {return document.createElement(e)};
-let WrapFullScreen = Id("WrapFullScreen");
 let quantity = document.getElementsByClassName('wrapperSpanSlide')[0].childNodes;
+let WrapFullScreen = document.getElementsByClassName('wrapperSpanSlide')[0];
+let quantityButton = Id('wIN').childNodes;
 let quantityLength = quantity.length-1; // Кількість слайдів
-let numberNextSlide = quantityLength;  // Номер наступного слайда
-let numberBeakSlide = quantityLength-1; // Номер основного слайда
-let zI = 1;                 // z-index
+let numberNextSlide = 1;  // Номер наступного слайда
+let numberBeakSlide = 1; // Номер основного слайда
+let zI = 8;                 // z-index
 let s1 = Id(`slide${numberNextSlide}`);
 let s2 = Id(`slide${numberBeakSlide}`); 
 for (let i = 0; i < quantityLength; i++) {
 	quantity[i].id = `slide${i+1}`;
 	let input = CE('input');
-		input.className = "inputSlide";
+		input.classList.add("inputSlide", "inputSlideN");
 		input.type="button";
-		input.value=`${i+1}`;
+		input.style.background = `url("${i+1}.jpg") `;
+		input.style.backgroundSize = `100% 100%`;
 		input.setAttribute('onclick', `fS1(${i+1})`);
-	Id('wIN').appendChild(input);	};
+	Id('wIN').appendChild(input);
+};
+quantity[0].style.zIndex = zI-1;
+console.log(quantityLength);
+console.log(quantityButton);
+
+function avtoHover(namber){
+	for (let i = 0; i < quantityLength; i++) {
+		quantityButton[i].style.boxShadow = 'inset 0 0 2em rgba(0, 0, 0, 0.3)';	}
+	quantityButton[namber].style.boxShadow = `inset 0 0 1em rgba(256, 256, 256, 0.3),  0em 0em 2em rgba(0, 0, 0, 1)`;};
+	avtoHover(numberBeakSlide-1);
 let setI = setInterval(() => {nextAuto();}, time);
 function beforeGo(e) {
 	s1 = Id(`slide${numberNextSlide}`);
-	if (s2 == null) {s2 = Id(`slide${quantityLength}`)};
+	avtoHover(numberNextSlide-1);
+	if (s2 == null) {s2 = Id(`slide${1}`)};
 	s1.style.cssText=`transition: 'left 0s';left: ${e}100%;`;
 	s1.style.zIndex = `${zI}`;
 	s2.style.cssText=`transition: 'left 0s';left: 0%;`;
@@ -69,8 +80,8 @@ function fS1(nNSU) {
 			s2 = s1;
 			beforeGo('-');
 		};
-	};};
-
+        };
+    };
 function clearIntervalMini() {
 	clearInterval(setI);
 	setI = setInterval(() => {
@@ -81,21 +92,15 @@ function pause() {
 	clearInterval(setI);
 }
 function toggleFullScreen() {
-	if (WrapFullScreen.mozRequestFullScreen) {
 	WrapFullScreen.mozRequestFullScreen();
 		if (WrapFullScreen.mozRequestFullScreen()) {} else {document.mozCancelFullScreen()}
-	}; // else document.exitFullscreen();
-	if (WrapFullScreen.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT)) {
 	WrapFullScreen.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
-} else {document.webkitCancelFullScreen()}};
-WrapFullScreen.addEventListener('keydown', function(event) {
-    console.log(event.keyCode);
+		if (WrapFullScreen.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT)) {} else {document.webkitCancelFullScreen()}
+};
+document.addEventListener('keydown', function(event) {
     if (event.keyCode==39) {nextAuto()};
     if (event.keyCode==37) {fS1()};
     if (event.keyCode==38) {toggleFullScreen()};
     if (event.keyCode==40) {toggleFullScreen()};
     if (event.keyCode==32) {pause()};
         }, false);
-function алерт(аргум) {
-	alert(аргум)
-}
